5dba4b805d621b575c7ace63c6752f13137aa1b0
vanim02 <vanim02@gmail.com>
2016-12-15 19:42:42 -0500

    moved to folder php

---
+++ php/login.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title> Login</title>
+
+</head>
+<body>
+<h3> Login </h3>
+<form name="loginform" method="POST" action="loginValidation.php">
+Username: <input type="text" id="loginuser" name="username" ></input><br>
+Password: <input type="password" id="loginpass" name="password" ></input><br>
+
+Login As (Role):   <select name="role">
+<option >Select</option>
+<option >Teacher</option>
+<option >Parent</option>
+	</select><br>
+ <input type="submit" name="submitlogin" id="login" value="Login"></input>
+</form>
+
+</body>
+</html>

2064b5ec62edb2f3ef9626f26f3720c88d680489
ravicm <ravicm@users.noreply.github.com>
2017-06-08 09:33:03 -0400

    Create MajorityElement.java

--- java/practice/src/levelSimple/MajorityElement.java
+++ java/practice/src/levelSimple/MajorityElement.java
@@ -19,6 +19,26 @@ public class MajorityElement {
 		HashMap<Integer, Integer> temp=new HashMap<Integer, Integer>();
 		int res=-1;
 		 		for(int i=0;i<num.length;i++){
+					/*
+					
+					Integer freq=temp.get(num[i]);
+			 		if(freq==null)
+			 			temp.put(num[i],1);
+			 		else{
+			 			if(freq+1>num.length/2)
+ 			 				return num[i];
+						temp.put(num[i],freq+1);
+ 			 		}
+					
+					difference to your code. 
+						a. only one lookup into map
+						b. use Integer (instead of int) to capture null. 
+							its a concept called boxing. google a bit and also part it
+						c. if condition can happen before write into table.
+					
+					*/
+					
+					
 			 		if(temp.get(num[i])==null)
 			 			temp.put(num[i],1);
 			 		else{
